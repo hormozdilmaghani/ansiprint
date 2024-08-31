80e8fea45165592eaf8f060dc46ceb7f72a8f795
hormozdilmaghani <hormozdilmaghani@users.noreply.github.com>
2024-08-31 23:50:42 +0100

    main

---
+++ ansiprint.py
@@ -0,0 +1,35 @@
+``TXT_BLACK           = '\u001b[30m'
+TXT_RED             = '\u001b[31m'
+TXT_GREEN           = '\u001b[32m'
+TXT_YELLOW          = '\u001b[33m' # orange on some systems
+TXT_BLUE            = '\u001b[34m'
+TXT_MAGENTA         = '\u001b[35m'
+TXT_CYAN            = '\u001b[36m'
+TXT_LIGHT_GRAY      = '\u001b[37m'
+TXT_DARK_GRAY       = '\u001b[90m'
+TXT_BRIGHT_RED      = '\u001b[91m'
+TXT_BRIGHT_GREEN    = '\u001b[92m'
+TXT_BRIGHT_YELLOW   = '\u001b[93m'
+TXT_BRIGHT_BLUE     = '\u001b[94m'
+TXT_BRIGHT_MAGENTA  = '\u001b[95m'
+TXT_BRIGHT_CYAN     = '\u001b[96m'
+TXT_WHITE           = '\u001b[97m'
+
+
+if __name__ == '__main__':
+    print(f'{TXT_BLACK}TXT_BLACK')       
+    print(f'{TXT_RED}TXT_RED')            
+    print(f'{TXT_GREEN}TXT_GREEN')       
+    print(f'{TXT_YELLOW}TXT_YELLOW')        
+    print(f'{TXT_BLUE}TXT_BLUE')          
+    print(f'{TXT_MAGENTA}G_MAGENTA')       
+    print(f'{TXT_CYAN}TXT_CYAN')          
+    print(f'{TXT_LIGHT_GRAY}TXT_LIGHT_GRAY')   
+    print(f'{TXT_DARK_GRAY}TXT_DARK_GRAY')     
+    print(f'{TXT_BRIGHT_RED}TXT_BRIGHT_RED')    
+    print(f'{TXT_BRIGHT_GREEN}TXT_BRIGHT_GREEN')  
+    print(f'{TXT_BRIGHT_YELLOW}TXT_BRIGHT_YELLOW') 
+    print(f'{TXT_BRIGHT_BLUE}TXT_BRIGHT_BLUE')   
+    print(f'{TXT_BRIGHT_MAGENTA}TXT_BRIGHT_MAGENTA')
+    print(f'{TXT_BRIGHT_CYAN}TXT_BRIGHT_CYAN')   
+    print(f'{TXT_WHITE}TXT_WHITE')
